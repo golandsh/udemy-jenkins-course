58cf48dfb5b8940bf0d3677420ff818612a1b7df
Shomi Goland <shlomi@agpulse.com>
2020-04-10 11:33:51 +0300

    edited git data

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/golandsh/udemy-docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL Shlomi Goland')
+            node / gitConfigEmail('golandsh@hotmail.com')
         }
     }
     triggers {
